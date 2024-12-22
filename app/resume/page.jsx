4be196dba83613577b6a18576412ff1c7d181d0e
Taiyob <oli1412001@gmail.com>
2024-12-22 18:58:44 +0600

    commit

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -2,6 +2,10 @@
 
 import { FaHtml5, FaCss3, FaJs, FaReact, FaFigma, FaNodeJs } from 'react-icons/fa'
 import { SiTailwindcss, SiNextdotjs } from 'react-icons/si'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/components/ui/tooltip'
+import { ScrollArea } from '@/components/ui/scroll-area'
+import { motion } from 'framer-motion'
 
 const about = {
     title: 'About me',
@@ -70,11 +74,104 @@ const experience = {
     ],
 }
 
+const education = {
+    icon: '/assets/resume/cap.svg',
+    title: 'My Education',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum necessitatibus obcaecati nobis.',
+    items: [
+        {
+            institution: 'Online Course Platform',
+            degree: 'Full Stack Web Development Bootcamp',
+            duration: '2023'
+        },
+        {
+            institution: 'Codeacademy',
+            degree: 'Front-end Track',
+            duration: '2022'
+        },
+        {
+            institution: 'Online Course',
+            degree: 'Programming Course',
+            duration: '2020 - 2021'
+        },
+        {
+            institution: 'Tech Institute',
+            degree: 'Certified Web Developer',
+            duration: '2019'
+        },
+        {
+            institution: 'Design School',
+            degree: 'Diploma in Grapic School',
+            duration: '2016 - 2018'
+        },
+    ]
+}
+
+const skills = {
+    title: 'My skills',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum necessitatibus obcaecati nobis.',
+    skillList: [
+        {
+            icon: <FaHtml5 />,
+            name: 'html 5'
+        },
+        {
+            icon: <FaCss3 />,
+            name: 'css3'
+        },
+        {
+            icon: <FaJs />,
+            name: 'javascript'
+        },
+        {
+            icon: <FaReact />,
+            name: 'react.js'
+        },
+        {
+            icon: <SiNextdotjs />,
+            name: 'next.js'
+        },
+        {
+            icon: <SiTailwindcss />,
+            name: 'tailwind.css'
+        },
+        {
+            icon: <FaNodeJs />,
+            name: 'node.js'
+        },
+        {
+            icon: <FaFigma />,
+            name: 'figma'
+        },
+    ]
+}
+
 const Resume = () => {
     return (
-        <div>
-            Resume page
-        </div>
+        <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1, transition: { delay: 2.4, duration: 0.4, ease: 'easeIn' } }}
+            className='min-h-[80vh] flex justify-center items-center py-12 xl:py-0'
+        >
+            <div className='container mx-auto'>
+                <Tabs
+                    defaultValue='experience'
+                    className='flex flex-col xl:flex-row gap-[60px]'
+                >
+                    <TabsList className='flex flex-col w-full max-w-[380px] mx-auto xl:mx-0 gap-6'>
+                        <TabsTrigger value='experience'>Experience</TabsTrigger>
+                        <TabsTrigger value='education'>Education</TabsTrigger>
+                        <TabsTrigger value='skills'>Skills</TabsTrigger>
+                        <TabsTrigger value='about'>About me</TabsTrigger>
+                    </TabsList>
+                    <div className='min-h-[70vh] w-full'>
+                        <TabsContent value='experience' className='w-full'>
+                            experience
+                        </TabsContent>
+                    </div>
+                </Tabs>
+            </div>
+        </motion.div>
     );
 };
 
